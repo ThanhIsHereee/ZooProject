6141f3ae67a803cbad907e260c1ba252871b11ec
ThanhIsHereee <nquangthanh88@gmail.com>
2023-11-06 00:18:23 +0700

    Upload 1st Project

---
+++ ZooRepo/LoginRepository.cs
@@ -0,0 +1,11 @@
+﻿using BusinessObject.Models;
+using ZooDAO;
+
+namespace ZooRepo
+{
+    public class LoginRepository : ILoginRepository
+    {
+        public User GetAccountByMail(string email)
+        =>LoginDAO.Instance.GetAccountByMail(email);
+    }
+}
