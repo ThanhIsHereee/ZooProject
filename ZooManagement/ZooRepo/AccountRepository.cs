6767845f59b04b3572f3aa80a5d671347b43a41c
ThanhIsHereee <nquangthanh88@gmail.com>
2023-11-06 14:00:37 +0700

    Upload Admin

---
+++ ZooManagement/ZooRepo/AccountRepository.cs
@@ -0,0 +1,25 @@
+﻿using BusinessObject.Models;
+using ZooDAO;
+
+namespace ZooRepo
+{
+    public class AccountRepository : IAccountRepository
+    {
+        public void AddAccount(User user)
+        =>AccountDAO.Instance.AddAccount(user);
+
+        public User GetAccountByID(string id)
+        =>AccountDAO.Instance.GetAccountByID(id);
+
+        public User GetAccountByMail(string email)
+        =>AccountDAO.Instance.GetAccountByMail(email);
+        public List<User> GetAllListUser()
+            =>AccountDAO.Instance.GetAllListUser();
+
+        public void RemoveAccount(string UserID)
+        =>AccountDAO.Instance.RemoveAccount(UserID);
+
+        public void UpdateAccount(User userID)
+        =>AccountDAO.Instance.UpdateAccount(userID);
+    }
+}
